82b70180448890793d288251c9284383f955b95f
jason whisnant <tasups@gmail.com>
2022-08-09 20:14:18 +0000

    small style added

--- src/data/blogsData.js
+++ src/data/blogsData.js
@@ -2,7 +2,7 @@ export const BLOG_DATA = [
   {
     id: 1,
     title: "How to Make Your Boss, and You, Happy!",
-    desc: "It's important that both you and your boss are happy. There are many, many reasons for this, however, here's a small list: good relations create non-toxic workplaces, if you make your boss happy you're more likely to get paid well, and it makes work easier.",
+    desc: "It's important that both you and your boss are happy. There are many, many reasons for this, however, for the sake of argument, here's a small list: good relations create non-toxic workplaces, if you make your boss happy you're more likely to get paid well, and it makes work easier.",
     img: "https://github.com/Tasups/images/blob/main/happyboss-blockimage.jpg?raw=true",
     img_alt: "happy boss"
   },
