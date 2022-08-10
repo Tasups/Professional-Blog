d9b54b1c255caf59eee940db6436e0755088c5e5
jason whisnant <tasups@gmail.com>
2022-08-10 11:26:06 +0000

    desc mapped over to differing p tags

--- src/components/BlogArticle.js
+++ src/components/BlogArticle.js
@@ -13,6 +13,11 @@ const BlogArticle = (props) => {
       </div>
       <article className="blogarticle-article">
         <h1>{props.blogArticle?.title}</h1>
+        {
+          props.blogArticle?.desc.split("\n").map((paragraph) => (
+            <p>{paragraph}</p>
+          ))
+        }
         <p>{props.blogArticle?.desc}</p>
       </article>
     </main>
