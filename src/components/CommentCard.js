8ec1de50f2512f0d536419a10ac10e2775ef543e
jason whisnant <tasups@gmail.com>
2022-08-15 13:31:48 +0000

    like button added and styled

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,12 +1,22 @@
+import { useState } from 'react'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 const CommentCard = (props) => {
 
   const { name, date, comment } = props.article;
+  let [countOfLikes, setCountOfLikes] = useState(0)
 
   const timestamp = date;
   const humanReadableDateTime = new Date(timestamp).toLocaleString();
+  
+ 
+  const handleClick = () => {
+    if(countOfLikes === 0) {
+      setCountOfLikes(countOfLikes => countOfLikes + 1)
+    }
+  }
 
   return (
     <div className="commentcard-container">
@@ -14,8 +24,19 @@ const CommentCard = (props) => {
         <h3>{name}</h3>
         <h5>{humanReadableDateTime}</h5>
         <p>{comment}</p>
-        <div>
-          <FontAwesomeIcon icon={faThumbsUp} />
+        <div className="commentcard-likescount">
+          <div className="commentcard-likescount-offset">
+          <button 
+            className="commentcard-thumbsup-btn"
+            onClick={handleClick}
+          >
+            <FontAwesomeIcon 
+              className="commentcard-icon"
+              icon={faThumbsUp} 
+            />
+          </button>
+          </div>
+          <span>{countOfLikes}</span>
         </div>
       </div>
     </div>
